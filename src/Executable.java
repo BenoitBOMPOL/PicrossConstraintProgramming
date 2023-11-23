248f65d12a5db120f7902753444ddf75645315da
Benoît BOMPOL <benoit.bompol@gmail.com>
2023-11-23 14:22:08 +0100

    add: example in src/Executable.java

--- src/Executable.java
+++ src/Executable.java
@@ -4,17 +4,23 @@ import java.util.Arrays;
 
 public class Executable {
     public static void main(String[] args) throws Exception{
-        picrossInstance bird = new picrossInstance("./picross/bird.px");
-        for (int row_id = 0; row_id < bird.getNb_rows(); row_id ++){
-            System.out.println("Allowed tuples for row no." + row_id + " under constraints " + Arrays.toString(bird.getRow_constraints(row_id)) + ".");
-            resolution(bird.getRow_constraints(row_id), bird.getNb_cols());
-        }
+        int row_size = 15;
+        int[] constaints = {3, 4, 2};
 
-        for (int col_id = 0; col_id < bird.getNb_cols(); col_id++){
-            System.out.println("Allowed tuples for col no." + col_id + " under constraints " + Arrays.toString(bird.getCol_constraints(col_id)) + ".");
-            resolution(bird.getCol_constraints(col_id), bird.getNb_rows());
-        }
+        resolution(constaints, row_size);
 
+        /**
+            picrossInstance bird = new picrossInstance("./picross/bird.px");
+            for (int row_id = 0; row_id < bird.getNb_rows(); row_id ++){
+                System.out.println("Allowed tuples for row no." + row_id + " under constraints " + Arrays.toString(bird.getRow_constraints(row_id)) + ".");
+                resolution(bird.getRow_constraints(row_id), bird.getNb_cols());
+            }
+
+            for (int col_id = 0; col_id < bird.getNb_cols(); col_id++){
+                System.out.println("Allowed tuples for col no." + col_id + " under constraints " + Arrays.toString(bird.getCol_constraints(col_id)) + ".");
+                resolution(bird.getCol_constraints(col_id), bird.getNb_rows());
+            }
+        */
 
     }
 
