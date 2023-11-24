1bcd8ce094456563b11a647954f5c66916137e8d
Benoît BOMPOL <benoit.bompol@gmail.com>
2023-11-24 21:17:14 +0100

    .

--- src/Executable.java
+++ src/Executable.java
@@ -6,7 +6,16 @@ public class Executable {
     public static void main(String[] args){
         String filename = "./picross/bird.px";
         try {
-            picrossInstance bird = new picrossInstance(filename);
+            picrossInstanceSlv bird = new picrossInstanceSlv(filename);
+            System.out.println("Instance has " + bird.getInstance().getNb_rows() + " rows and " + bird.getInstance().getNb_cols() + " columns.");
+            System.out.println("Constraints on rows :");
+            for (int row_id = 0; row_id < bird.getInstance().getNb_rows(); row_id++){
+                System.out.println("\t Row no." + row_id + " : " + Arrays.toString(bird.getInstance().getRow_constraints(row_id)));
+            }
+            System.out.println("Constraints on cols :");
+            for (int col_id = 0; col_id < bird.getInstance().getNb_cols(); col_id++){
+                System.out.println("\t col no." + col_id + " : " + Arrays.toString(bird.getInstance().getCol_constraints(col_id)));
+            }
         } catch (Exception e){
             e.printStackTrace();
         }
--- src/structs/picrossInstanceSlv.java
+++ src/structs/picrossInstanceSlv.java
@@ -1,89 +1,40 @@
 package structs;
-
 import ilog.cp.IloCP;
 import ilog.concert.*;
-
 public class picrossInstanceSlv {
     private IloCP solver;
     private picrossInstance instance;
-    private int[][][] row_allowed_tuples;
-    private int[][][] col_allowed_tuples;
 
-    private IloIntTupleSet[] rows;
-    private IloIntTupleSet[] cols;
-    private IloIntVar[][] effectiveRows;
-    private IloIntVar[][] effectiveCols;
+    private int[][][] allowedtuples_inrows;
+    private int[][][] allowedtuples_incols;
 
     public picrossInstance getInstance() {
         return instance;
     }
-
-    public int[][] getCol_allowed_tuples(int j) {
-        return col_allowed_tuples[j];
+    public int [][] get_row_allowedtuples(int i){
+        return allowedtuples_inrows[i];
+    }
+    public int [][] get_col_allowedtuples(int j){
+        return allowedtuples_incols[j];
     }
 
-    public int[][] getRow_allowed_tuples(int i) {
-        return row_allowed_tuples[i];
-    }
-
-    public picrossInstanceSlv(String filename) throws Exception{
-        this.instance = new picrossInstance(filename);
-        this.row_allowed_tuples = new int[instance.getNb_rows()][][];
-        this.col_allowed_tuples = new int[instance.getNb_cols()][][];
-
-        for (int row_id = 0; row_id < instance.getNb_rows(); row_id++){
-            AllowedTupleSearcher row_search =
-                    new AllowedTupleSearcher(instance.getRow_constraints(row_id),instance.getNb_cols());
-            row_allowed_tuples[row_id] = row_search.getAllSolutions();
-        }
-
-        for (int col_id = 0; col_id < instance.getNb_cols(); col_id++){
-            AllowedTupleSearcher col_search =
-                    new AllowedTupleSearcher(instance.getCol_constraints(col_id), instance.getNb_rows());
-            col_allowed_tuples[col_id] = col_search.getAllSolutions();
-        }
-
-        this.rows = new IloIntTupleSet[instance.getNb_rows()];
-        this.cols = new IloIntTupleSet[instance.getNb_cols()];
-
-        this.effectiveRows = new IloIntVar[instance.getNb_rows()][];
-        for (int row_id = 0; row_id < instance.getNb_rows(); row_id++){
-            this.effectiveRows[row_id] = new IloIntVar[instance.getRow_constraints(row_id).length];
-        }
-        this.effectiveCols = new IloIntVar[instance.getNb_cols()][];
-        for (int col_id = 0; col_id < instance.getNb_rows(); col_id++){
-            this.effectiveRows[col_id] = new IloIntVar[instance.getCol_constraints(col_id).length];
-        }
-        stateModel();
-    }
-
-    public void stateModel(){
-        try{
-            solver = new IloCP();
-            solver.setParameter(IloCP.IntParam.LogVerbosity, IloCP.ParameterValues.Quiet);
-            solver.setParameter(IloCP.IntParam.SearchType, IloCP.ParameterValues.DepthFirst);
-            solver.setParameter(IloCP.IntParam.Workers, 1);
+    public picrossInstanceSlv(String filename){
+        try {
+            this.instance = new picrossInstance(filename);
+            this.allowedtuples_inrows = new int[instance.getNb_rows()][][];
+            this.allowedtuples_incols = new int[instance.getNb_cols()][][];
 
             for (int row_id = 0; row_id < instance.getNb_rows(); row_id++){
-                rows[row_id] = solver.intTable(instance.getRow_constraints(row_id).length);
-                for (int[] valid_row : row_allowed_tuples[row_id]){
-                    solver.addTuple(rows[row_id], valid_row);
-                }
-                solver.add(solver.allowedAssignments(effectiveRows[row_id], rows[row_id]));
+                AllowedTupleSearcher rowsearch = new AllowedTupleSearcher(instance.getRow_constraints(row_id), instance.getNb_cols());
+                allowedtuples_inrows[row_id] = rowsearch.getAllSolutions();
             }
 
-            for (int col_id = 0; col_id < instance.getNb_rows(); col_id++){
-                rows[col_id] = solver.intTable(instance.getCol_constraints(col_id).length);
-                for (int[] valid_col : row_allowed_tuples[col_id]){
-                    solver.addTuple(rows[col_id], valid_col);
-                }
-                solver.add(solver.allowedAssignments(effectiveCols[col_id], cols[col_id]));
+            for (int col_id = 0; col_id < instance.getNb_cols(); col_id++){
+                AllowedTupleSearcher colsearch = new AllowedTupleSearcher(instance.getCol_constraints(col_id), instance.getNb_rows());
+                allowedtuples_incols[col_id] = colsearch.getAllSolutions();
             }
-
-            // TODO
-        } catch (IloException e){
+        } catch (Exception e){
             e.printStackTrace();
         }
-
     }
 }
